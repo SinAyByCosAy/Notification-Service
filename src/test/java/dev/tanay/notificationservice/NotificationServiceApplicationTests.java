ecfdca6cd66e3c26a27d916be4ff407ee4ef82f4
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-30 02:54:40 +0530

    base setup

---
+++ src/test/java/dev/tanay/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tanay.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
